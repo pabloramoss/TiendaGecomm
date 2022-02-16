ab1b27bf6ff4523c9255c8bca93c1b971c581d5b
pablo-pc <pablo-362@hotmail.com>
2022-02-16 11:56:38 -0300

    fixed whatsapp number in logo link and link in logo

--- components/ui/NavbarCheckout.jsx
+++ components/ui/NavbarCheckout.jsx
@@ -9,8 +9,10 @@ function NavbarCheckout() {
     <Stack py={3} bg="white" pos="fixed" w="100%" zIndex={100}>
       <Container maxW="container.xl">
         <HStack justifyContent="space-between" alignItems="end">
-          <Image src="/images/gecomm-logo.png" alt="gecomm logo" />
-          <Link href="https://api.whatsapp.com/send?phone=543424270884&message" isExternal>
+          <Link href='/'>
+            <Image src="/images/gecomm-logo.png" alt="gecomm logo" />
+          </Link>
+          <Link href="https://api.whatsapp.com/send?phone=5493426483165&message" isExternal>
             <Icon w={10} h={10} as={FaWhatsappSquare} color="green.400" />
           </Link>
         </HStack>

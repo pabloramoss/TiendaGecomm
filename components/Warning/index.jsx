dd7dfe7fa50a5f46b144c492ad49d78130505dd2
pablo-pc <pablo-362@hotmail.com>
2022-07-24 19:33:32 -0300

    fix warning component

--- components/Warning/index.jsx
+++ components/Warning/index.jsx
@@ -1,13 +1,15 @@
 import { Stack, Text } from '@chakra-ui/react';
 import React from 'react';
-import { FaExclamation, FaExclamationCircle } from 'react-icons/fa';
+import { FaExclamationCircle } from 'react-icons/fa';
 
 const Warning = ()=> {
 
   return(
-    <Stack  justifyContent="center" alignItems="center">
-      
-      <Text background="red.200" padding={5} display="flex" gap={5}><FaExclamationCircle size={20} />Por razones de público conocimiento momentáneamente realizamos las ventas únicamente por nuestros medios de comunicación. Por favor, contáctese con un vendedor.</Text>
+    <Stack justifyContent="center" alignItems="center">
+      <Stack mt={{base: "100px", lg: "0"}} p={5} gap={5} direction="row" alignItems="center" background="red.200">
+        <FaExclamationCircle size={40} />
+        <Text fontSize={{base: "10", md: "15"}} fontWeight={500} display="flex" alignItems="center" gap={5}>Por razones de público conocimiento momentáneamente realizamos las ventas únicamente por nuestros medios de comunicación. Por favor, contáctese con un vendedor.</Text>
+      </Stack>
     </Stack>
   )
 }
